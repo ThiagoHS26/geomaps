7079f3a4b37f295231b3c9e5a26ac6d0579bd445
ThiagoHS26 <thiagotester8@gmail.com>
2022-12-01 15:52:30 -0500

    project add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoFoundPageComponent } from './no-found-page/no-found-page.component';
+import { AuthModule } from './auth/auth.module';
+import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoFoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
+import { PagesComponent } from './pages.component';
+
+const routes:Routes = [
+  {path:'dashboard',component:PagesComponent,
+    children:[
+      {path:'mapas',component:DashboardComponent},
+      {path:'usuarios',component:UsuariosComponent}
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class PagesRoutingModule { }
